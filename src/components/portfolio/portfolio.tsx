407035927cfe55eabca77b7f8c6872e1ff280478
Saman <119083568+navidasaman@users.noreply.github.com>
2024-04-11 12:50:08 +0200

    Refactored/Cleaner code
    data and logic will be seperated

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -24,6 +24,33 @@ function Portfolio() {
     window.open("https://github.com/navidasaman", "_blank", "noopener noreferrer");
   };
 
+  const skills = [
+    { title: "HTML", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg", alt: "HTML5 SVG icon" },
+    { title: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg", alt: "CSS SVG icon" },
+    { title: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg", alt: "JavaScript SVG icon" },
+    { title: "TypeScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg", alt: "TypeSCript SVG icon" },
+    { title: "React", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg", alt: "React SVG icon" },
+    { title: "MySQL", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg", alt: "MySQL SVG icon" },
+    { title: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg", alt: "Node.js SVG icon" },
+    { title: "Java", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg", alt: "Java SVG icon" },
+    { title: "PHP", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg", alt: "PHP SVG icon" },
+    { title: "C++", icon: cpp, alt: "C++ icon" },
+    { title: "SASS/SCSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg", alt: "SASS SVG icon" },
+    { title: "Bootstrap", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg", alt: "Bootstrap SVG icon" },
+    { title: "TailwindCSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg", alt: "TailwindCSS SVG icon" },
+    { title: "D3.JS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/d3js/d3js-original.svg", alt: "D3,js SVG icon" },
+    { title: "Git", icon: "ttps://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg", alt: "Git SVG icon" },
+    { title: "GitHub", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg", alt: "GitHub SVG icon" },
+    { title: "FileZilla", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/filezilla/filezilla-plain.svg", alt: "FileZilla SVG icon" },
+    { title: "Markdown", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg", alt: "Markdown SVG icon" },
+    { title: "InkScape", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/inkscape/inkscape-plain-wordmark.svg", alt: "InkScape SVG icon" },
+    { title: "Visual Studio Code", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-plain-wordmark.svg", alt: "Visual Studio Code SVG icon" },
+    { title: "ExpressJS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg", alt: "ExpressJS SVG icon" },
+    { title: "Android Studio", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original-wordmark.svg", alt: "Android Studio SVG icon" },
+    { title: "Postman", icon: postman, alt: "Postman SVG icon" },
+    { title: "RESTful API", icon: restapi, alt: "REST API SVG icon" },
+  ];
+
   return (
     <div className='portfolioContainer'>
       {/*Intro*/}
@@ -43,9 +70,9 @@ function Portfolio() {
         < Gallery />
       </div>
       <p className="portfolioParagraphB">Here are some of the projects I've worked on. Feel free to scroll through them. To see more visit my:</p>
-       <button onClick={handleGithubLink} className="button">
-          <span className="text">GitHub |</span> <span className="icon"> <BsGithub  /></span>        
-      </button> 
+      <button onClick={handleGithubLink} className="button">
+        <span className="text">GitHub |</span> <span className="icon"> <BsGithub /></span>
+      </button>
       {/* <hr style={{ backgroundColor: 'aqua', marginTop: '5%', borderRadius: '50%', opacity: '0.1' }} /> */}
       {/*Language/Frameworks/Libraries/Softwares Experience*/}
       <div className="experienceInContainer">
@@ -63,33 +90,13 @@ function Portfolio() {
         </Root>
       </div>
       <div className="skills" data-aos="fade-up">
-        <Tooltip title="HTML" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt="html5 icon svg" /></Tooltip>
-        <Tooltip title="CSS" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="css3 icon svg" /></Tooltip>
-        <Tooltip title="JavaScript" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt="javascript icon svg" /></Tooltip>
-        <Tooltip title="TypeScript" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" alt="typescript icon svg" /></Tooltip>
-        <Tooltip title="React" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react icon svg" /></Tooltip>
-        <Tooltip title="MySQL" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" alt="mysql icon svg" /></Tooltip>
-        <Tooltip title="Node.js" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs icon svg" /></Tooltip>
-        <Tooltip title="Java" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="java icon svg" /></Tooltip>
-        <Tooltip title="PHP" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" alt="php icon svg" /></Tooltip>
-        <Tooltip title="C++" arrow><img src={cpp} alt="C++ icon" ></img></Tooltip>
-        <Tooltip title="SASS/SCSS" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt="sass icon svg" /></Tooltip>
-        <Tooltip title="Bootstrap" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt="bootstrap icon svg" /></Tooltip>
-        <Tooltip title="TailwindCSS" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="TailwindCSS icon svg" /></Tooltip>
-        <Tooltip title="D3.js" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/d3js/d3js-original.svg" alt="d3.js icon svg" /></Tooltip>
-        <Tooltip title="Git" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" alt="git icon svg" /></Tooltip>
-        <Tooltip title="GitHub" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="github icon svg" /></Tooltip>
-        <Tooltip title="FileZilla" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/filezilla/filezilla-plain.svg" alt="filezilla icon svg" /></Tooltip>
-        <Tooltip title="Markdown" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg" alt="markdown icon svg" /></Tooltip>
-        <Tooltip title="InkScape" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/inkscape/inkscape-plain-wordmark.svg" alt="inkscape icon svg" /></Tooltip>
-        <Tooltip title="Visual Studio Code" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-plain-wordmark.svg" alt="visual studio code icon svg" /></Tooltip>
-        <Tooltip title="ExpressJS" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express.js icon svg" /></Tooltip>
-        <Tooltip title="Android Studio" arrow><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original-wordmark.svg" alt="android studio code icon svg" /></Tooltip>
-        <Tooltip title="Postman" arrow><img src={postman} alt="postman icon" ></img></Tooltip>
-        <Tooltip title="RESTful API" arrow><img src={restapi} alt="resp icon" ></img></Tooltip>
-        
-
+        {skills.map((skill, index) => (
+          <Tooltip key={index} title={skill.title} arrow>
+            <img src={skill.icon} alt={skill.alt} />
+          </Tooltip>
+        ))}
       </div>
+
     </div>
   )
 }
